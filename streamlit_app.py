cb53ccb77ecedacf8c6b76d2c85554a3ec155134
Jbonc <115024939+Jbonc@users.noreply.github.com>
2022-10-07 12:37:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('🥣  My Parents New Healthy Diner')
 streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal') 
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑Hard-Boiled Free-Range Egg')
 import pandas 
